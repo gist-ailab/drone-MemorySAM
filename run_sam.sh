d8626d519676ec3700c161d6edf9b30318039c7d
Oscar Liao <106236413+Chenfei-Liao@users.noreply.github.com>
2025-03-13 19:00:29 +0800

    Update run_sam.sh

--- run_sam.sh
+++ run_sam.sh
@@ -1,7 +1,7 @@
 export cur_dir=`pwd`
 export save_exp_name="0917_DELIVER_RGBDepth_SAM2_bplus_lora_lr00008"
 export CUDA_VISIBLE_DEVICES="0"
-python3 -m torch.distributed.launch --nproc_per_node=1 --master_port=21612 --use_env train_mm_sam.py \
+python3 -m torch.distributed.launch --nproc_per_node=1 --master_port=21612 --use_env train_sam2_lora.py \
  --cfg /hpc2hdd/home/cliao127/MMSS-SAM-S1/configs/xxxxx.yaml \
 \
 2>&1 \
