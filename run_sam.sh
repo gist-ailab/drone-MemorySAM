3dd446bad2bf2dc1b74eeeb219f996b6a301deb3
maengjemo <maengjemo@gm.gist.ac.kr>
2026-02-15 10:56:02 +0900

    UPddate local commit  levine, bash

--- run_sam.sh
+++ run_sam.sh
@@ -1,13 +1,13 @@
 export cur_dir=`pwd`
-export save_exp_name="0206_DELIVER_RGBDEL_LORA_SAM2_P8_lora_lr00006"
-export CUDA_VISIBLE_DEVICES="0,1,2,3"
+export save_exp_name="0209_DELIVER_RGBDE_LORA_SAM2_P8_lora_lr00006"
+export CUDA_VISIBLE_DEVICES="0,1,2,3,4,5,6,7"
 
-folder_name="0206_DELIVER_RGBDEL_LORA_SAM2_P8_lora_lr00006"
+folder_name="0209_DELIVER_RGBDE_LORA_SAM2_P8_lora_lr00006"
 mkdir -p logs/${folder_name}
 timestamp=$(date +%Y%m%d_%H%M%S)
 
 
-python3 -u -m torch.distributed.launch --nproc_per_node=4 --master_port=21612 --use_env train_sam2_lora_paper.py \
- --cfg configs/hpc_deliver_rgbdel_P8.yaml \
+python3 -u -m torch.distributed.launch --nproc_per_node=8 --master_port=21612 --use_env train_sam2_lora_paper.py \
+ --cfg configs/levine-deliver_rgbde_P8.yaml \
 2>&1 | tee "logs/${folder_name}/${save_exp_name}_${timestamp}.log"
 
